5e120856732dff4d65e8e69cdb18d56762e1f93a
dawiddszewczyk <dawsze567@wp.pl>
2020-05-25 12:09:37 +0200

    Update typed.dart

--- animacion_flutter/lib/typed.dart
+++ animacion_flutter/lib/typed.dart
@@ -11,28 +11,36 @@ class _TypedState extends State<Typed> with TickerProviderStateMixin{
 
   void initState(){
     animationController1 = AnimationController(
-      duration: (Duration(milliseconds: 600)),
+      duration: (Duration(milliseconds: 900)),
       vsync: this,
     );
     animationController2 = AnimationController(
-      duration: (Duration(milliseconds: 600)),
+      duration: (Duration(milliseconds: 900)),
       vsync: this,
     );
 
     animation = Tween(
       begin: 4.0,
       end: 0.0,
-    ).animate(animationController1)..addListener((){
-      setState(() {
-      });
-    });
+    ).animate(CurvedAnimation(
+      parent: animationController1,
+      curve: Interval(
+        0,
+        1.0,
+        curve: Curves.decelerate,
+      ),
+    ));
     animation2 = Tween(
       begin: 0.0,
       end: 4.0,
-    ).animate(animationController2)..addListener((){
-      setState(() {
-      });
-    });
+    ).animate(CurvedAnimation(
+      parent: animationController2,
+      curve: Interval(
+        0,
+        1.0,
+        curve: Curves.decelerate,
+      ),
+    ));
     animationController1.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
         animationController1.reverse();
@@ -47,9 +55,9 @@ class _TypedState extends State<Typed> with TickerProviderStateMixin{
         animationController2.forward();
       }
     });
-    Future.delayed(Duration(milliseconds: 300),
+    Future.delayed(Duration(milliseconds: 200),
             () => animationController1.forward());
-    Future.delayed(Duration(milliseconds: 300),
+    Future.delayed(Duration(milliseconds: 200),
             () => animationController2.forward());
     super.initState();
   }
@@ -70,59 +78,79 @@ class _TypedState extends State<Typed> with TickerProviderStateMixin{
           ),
           Row(
             children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Container(
-                    transform: Matrix4.translationValues(0.0, animation2.value*5, 0.0),
-                    height: 10,
-                    width: 10,
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(49,81,183,1),
-                      shape: BoxShape.circle,
+              Padding(
+                padding: EdgeInsets.only(top: 8,left: 8,bottom: 12),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      transform: Matrix4.translationValues(0.0, animation2.value*5, 0.0),
+                      height: 5,
+                      width: 5,
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(49,81,183,1),
+                        shape: BoxShape.circle,
+                      ),
                     ),
-                  ),
-                  Container(
-                    transform: Matrix4.translationValues(0.0, 10, 0.0),
-                    height: 10,
-                    width: 10,
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(49,81,183,1),
-                      shape: BoxShape.circle,
+                    Container(
+                      transform: Matrix4.translationValues(0.0, 10, 0.0),
+                      height: 5,
+                      width: 5,
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(49,81,183,1),
+                        shape: BoxShape.circle,
+                      ),
                     ),
-                  ),
-                  Container(
-                    transform: Matrix4.translationValues(0.0, animation.value*5, 0.0),
-                    height: 10,
-                    width: 10,
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(49,81,183,1),
-                      shape: BoxShape.circle,
+                    Container(
+                      transform: Matrix4.translationValues(0.0, animation.value*5, 0.0),
+                      height: 5,
+                      width: 5,
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(49,81,183,1),
+                        shape: BoxShape.circle,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
               SizedBox(width: 20),
               Padding(
                 padding: EdgeInsets.only(top:12),
-                child: Container(
-                  width: 200,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(244,244,244,1),
-                    borderRadius: BorderRadius.only(
-                        topLeft:  Radius.circular(40.0),
-                        topRight: Radius.circular(40.0),
-                        bottomLeft:  Radius.circular(40.0),
-                        bottomRight: Radius.circular(40.0)
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      transform: Matrix4.translationValues(-100.0, 0.0, 0.0),
+                    height: 1,
+                    width: 300,
+                      color: Colors.red,
                     ),
-                  ),
-                  child: Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(14.0),
-                      child: Text("Watson is responding...",
-                          style: TextStyle(color: Color.fromRGBO(118, 118, 118,1),fontSize: 16)),
+                    Container(
+                      transform: Matrix4.translationValues(-50.0, 0.0, 0.0),
+                      width: 200,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(244,244,244,1),
+                        borderRadius: BorderRadius.only(
+                            topLeft:  Radius.circular(40.0),
+                            topRight: Radius.circular(40.0),
+                            bottomLeft:  Radius.circular(40.0),
+                            bottomRight: Radius.circular(40.0)
+                        ),
+                      ),
+                      child: Center(
+                        child: Padding(
+                          padding: EdgeInsets.all(14.0),
+                          child: Text("Watson is responding...",
+                              style: TextStyle(color: Color.fromRGBO(118, 118, 118,1),fontSize: 16)),
+                        ),
+                      ),
                     ),
-                  ),
+                    Container(
+                      transform: Matrix4.translationValues(-100.0, 0.0, 0.0),
+                      height: 1,
+                      width: 300,
+                      color: Colors.red,
+                    ),
+                  ],
                 ),
               ),
             ],
